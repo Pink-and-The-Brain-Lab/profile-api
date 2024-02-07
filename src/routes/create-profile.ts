c21888951a1b97c3a5a8826acd523e7b3aa49dbf
André Lagacione <lagacioneandre@gmail.com>
2024-02-06 21:14:42 -0500

    upload images

--- src/routes/create-profile.ts
+++ src/routes/create-profile.ts
@@ -1,17 +1,22 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { IProfile } from "./interfaces/proifle.inteface";
 import AppError from "../errors/AppError";
 import CreateProfileService from "../services/CreateProfileService";
 import validateUserToken from "../utils/validate-user-token";
+import multer from 'multer';
+import uploadConfig from "../utils/upload-config";
+const upload = multer(uploadConfig);
 
 const createProfileRouter = Router();
 
-createProfileRouter.post('/', async (request: Request<IProfile>, response: Response, next: NextFunction) => {
+createProfileRouter.post('/', upload.single('image'), async (request: Request, response: Response, next: NextFunction) => {
     try {
         const userTokenData = await validateUserToken(request, response);
+        // console.log(request.body);
+        const imageUrl = request.file?.filename || '';
         const data = {
             ...request.body,
-            userId: userTokenData.sub
+            userId: userTokenData.sub,
+            image: imageUrl,
         };
         const service = await new CreateProfileService().execute(data);
         if (service.statusCode) throw new AppError(service.message || '', service.statusCode);
