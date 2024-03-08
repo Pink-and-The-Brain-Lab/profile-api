3c36633c0721f90ab2141d3877c5df1dfe3fee85
André Lagacione <lagacioneandre@gmail.com>
2024-03-08 15:47:26 -0500

    add web socket

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,4 @@
 import app from "./app";
+import { socketIo } from "./web-socket";
 app.listen(3003, () => console.log("users API started on port 3003!"));
+socketIo.listen(5000, () => console.log('web socket listen on port 5000!'));
