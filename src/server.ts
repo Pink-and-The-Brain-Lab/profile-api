5c0fbb728054939de5368b58ad8679c67735d9cb
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2024-12-03 08:41:55 -0300

    add rabbit listeners

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import app from "./app";
+import RabbitListener from "./services/RabbitListener";
 import { socketIo } from "./web-socket";
-app.listen(3003, () => console.log("users API started on port 3003!"));
+app.listen(3003, () => {
+    console.log("users API started on port 3003!");
+    new RabbitListener().listeners();
+});
 socketIo.listen(5000, () => console.log('web socket listen on port 5000!'));
