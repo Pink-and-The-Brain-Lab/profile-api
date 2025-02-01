326431aea79a8e29e4b25bcbf4bba0f643eca633
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-02-01 10:50:14 -0300

    change to use env variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,22 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { UserProfile1706998803968 } from "./database/migrations/1706998803968-UserProfile";
 import Profile from "./models/profile.model";
 import { AddColumnSelected1708562189770 } from "./database/migrations/1708562189770-AddColumnSelected";
 import { AddColumnLanguage1709500814057 } from "./database/migrations/1709500814057-AddColumnLanguage";
 import { AddColumnCreatedAt1709927126156 } from "./database/migrations/1709927126156-AddColumnCreatedAt";
 
+if (process.env.NODE_ENV === 'test') {
+    config({ path: './.env.test' });
+} else {
+    config();
+}
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5434,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT as string, 10),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: "postgres",
@@ -26,4 +32,4 @@ export const AppDataSource = new DataSource({
         AddColumnCreatedAt1709927126156,
     ],
     subscribers: [],
-})
+});
