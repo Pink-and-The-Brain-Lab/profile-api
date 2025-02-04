db5b7dcfb6067d785388c9b0f80f431d04ca95b6
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-02-04 08:03:29 -0300

    create get token data rabbitmq connection

--- src/constants/get-token-data.ts
+++ src/constants/get-token-data.ts
@@ -1,3 +1,4 @@
 import { GetTokenData } from "millez-lib-api";
 import { RabbitMqQueues } from "../enums/rabbitmq-queues.enum";
-export const GET_TOKEN_DATA = new GetTokenData(RabbitMqQueues.VALIDATE_USER_SESSION, RabbitMqQueues.PROFILE_RESPONSE_QUEUE);
+import { RABBITMQ_HOST_URL } from "./rabbitmq-host-url";
+export const GET_TOKEN_DATA = new GetTokenData(RabbitMqQueues.VALIDATE_USER_SESSION, RabbitMqQueues.PROFILE_RESPONSE_QUEUE, RABBITMQ_HOST_URL);
