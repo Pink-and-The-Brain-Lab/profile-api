03fc23b9ece71eaaef71928dc762ae2e24b62c9e
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-02-05 07:48:22 -0300

    create validate token rabbitmq connection

--- src/constants/validate-token.ts
+++ src/constants/validate-token.ts
@@ -1,3 +1,4 @@
 import { ValidateToken } from "millez-lib-api";
 import { RabbitMqQueues } from "../enums/rabbitmq-queues.enum";
-export const VALIDATE_TOKEN = new ValidateToken(RabbitMqQueues.VALIDATE_USER_SESSION, RabbitMqQueues.PROFILE_RESPONSE_QUEUE);
+import { RABBITMQ_HOST_URL } from "./rabbitmq-host-url";
+export const VALIDATE_TOKEN = new ValidateToken(RabbitMqQueues.VALIDATE_USER_SESSION, RabbitMqQueues.PROFILE_RESPONSE_QUEUE, RABBITMQ_HOST_URL);
